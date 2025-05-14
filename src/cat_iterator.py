f53e0a17a061dd45f11291355bd770ae8b9b0576
Egozzar <Egozzar@yandex.ru>
2025-05-14 18:04:23 +0300

    added cat_iterator.py with class CatIterator @ test_cat_iterator_py

---
+++ src/cat_iterator.py
@@ -0,0 +1,39 @@
+from typing import Iterator
+
+from src.category import Category
+from src.product import Product
+
+
+class CatIterator:
+    """
+    Шаблон для создания объектов для перебора продуктов одной категории
+    """
+
+    def __init__(self, category: Category) -> None:
+        """
+        Метод для инициализации итератора. Задаем значения атрибутам экземпляра.
+        :param category: (Category) экземпляр класса Category
+        """
+        self.category = category
+
+    def __iter__(self) -> Iterator:
+        """
+        Метод возвращает итератор класса
+        :return: (Iterator)
+        """
+        self.runner = 0
+
+        return self
+
+    def __next__(self) -> Product:
+        """
+        Метод возвращает продукт перебираемой категории
+        :return: (Product)
+        """
+        if self.runner < len(self.category.products):
+            elem = self.category.products[self.runner]
+            self.runner += 1
+
+            return elem
+
+        raise StopIteration
---
+++ tests/test_cat_iterator.py
@@ -0,0 +1,20 @@
+import pytest
+
+
+def test_catiterator_init(category_iterator):
+    assert category_iterator.category.description == "Rich in vitamins and always fresh"
+    assert category_iterator.category.name == "Vegetables"
+
+
+def test_catiterator_iter(category_iterator):
+    assert iter(category_iterator).runner == 0
+
+
+def test_catiterator_next(category_iterator):
+    iter(category_iterator)
+    assert next(category_iterator) == "Tomato, 250.0 руб. Остаток: 10 шт."
+    assert next(category_iterator) == "Pepper, 200.0 руб. Остаток: 15 шт."
+    assert next(category_iterator) == "Cucumber, 160.5 руб. Остаток: 20 шт."
+
+    with pytest.raises(StopIteration):
+        next(category_iterator)
